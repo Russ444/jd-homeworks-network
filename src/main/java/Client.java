13f5b6fb1aeb1cf6c06c60c3819e85301371d284
“Ruslan” <borisovr-n@yandex.ru>
2024-04-15 13:29:09 +0300

    Задание исправлено

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,15 +6,13 @@ public class Client {
         String serverAddress = "netology.homework";
         int serverPort = 8080;
 
-        try {
-            Socket socket = new Socket(serverAddress, serverPort);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        try (Socket socket = new Socket(serverAddress, serverPort);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader keyboardInput = new BufferedReader(new InputStreamReader(System.in))) {
 
             String message = in.readLine();
             System.out.println("Сервер отвечает: " + message);
-
-            BufferedReader keyboardInput = new BufferedReader(new InputStreamReader(System.in));
 
             System.out.print("Введите ваше имя: ");
             String name = keyboardInput.readLine();
@@ -23,14 +21,13 @@ public class Client {
             message = in.readLine();
             System.out.println("Сервер спрашивает: " + message);
 
-            System.out.print("Вы ребенок? (yes/no): ");
+            System.out.print("Вы ребенок? (да/нет): ");
             String answer = keyboardInput.readLine();
             out.println(answer);
 
             message = in.readLine();
             System.out.println("Сервер отвечает: " + message);
 
-            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,34 +4,33 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) {
         int port = 8080;
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту " + port);
 
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Новое подключение принято с порта " + clientSocket.getPort());
+                try (Socket clientSocket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    out.println("Привет! Назови свое имя?");
+                    String name = in.readLine();
+                    System.out.println("Имя клиента: " + name);
 
-                out.println("Привет! Назови свое имя?");
-                String name = in.readLine();
-                System.out.println("Имя клиента: " + name);
+                    out.println("Добро пожаловать, " + name + "! Ты ребенок? (да/нет)");
+                    String isChild = in.readLine();
+                    System.out.println("Клиент ответил: " + isChild);
 
-                out.println("Добро пожаловать, " + name + "! Ты ребенок? (да/нет)");
-                String isChild = in.readLine();
-                System.out.println("Клиент ответил: " + isChild);
+                    String response;
+                    if (isChild.equalsIgnoreCase("да")) {
+                        response = "Добро пожаловать в детскую зону, " + name + "! Давай поиграем!";
+                    } else {
+                        response = "Добро пожаловать в зону для взрослых, " + name + "! Приятного отдыха или продуктивного рабочего дня!";
+                    }
+                    out.println(response);
 
-                String response;
-                if (isChild.equalsIgnoreCase("да")) {
-                    response = "Добро пожаловать в детскую зону, " + name + "! Давай поиграем!";
-                } else {
-                    response = "Добро пожаловать в зону для взрослых, " + name + "! Приятного отдыха или продуктивного рабочего дня!";
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                out.println(response);
-
-                clientSocket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
